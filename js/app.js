bfbea0d62e80245c2602f686ae267d29e188b360
axetrodome <axetrodome@yahoo.com>
2017-11-17 17:12:48 +0800

    add link

--- js/app.js
+++ js/app.js
@@ -1,13 +1,42 @@
-var todoModal = document.getElementById('todoModal')
-var todoBtn = document.getElementById('todoBtn')
+function Open(event){
+	let todoModal = document.getElementById('todoModal'),
+		todoBtn = document.getElementById('todoBtn')
 
-todoBtn.addEventListener('click',Open)
-
-function Open(){
 	if(todoModal.className === 'todo-wrapper'){
-
 		todoModal.className += ' show';
 	}else{
 		todoModal.className = 'todo-wrapper'
 	}
 }
+function openLink(event){
+	let linkModal = document.getElementById('linkModal'),
+		linkBtn = document.getElementById('linkBtn')
+
+	if(linkModal.className === 'link-wrapper'){
+		linkModal.className += ' show';
+	}else{
+		linkModal.className = 'link-wrapper'
+	}
+}
+(function updateClock(){
+	let now = new Date(),
+	    months = [	'January', 
+			        'February', 
+			        'March',
+			        'April',
+			        'May',
+			        'June',
+			        'July',
+			        'August',
+			        'September',
+			        'October',
+			        'November',
+			        'December'
+	    		],
+	hours = ((now.getHours() + 11) % 12 + 1)
+	    		
+	time = hours + ':' +now.getMinutes()
+
+	document.getElementById('time').innerHTML = time
+	setTimeout(updateClock,1000)
+})(); //invoking itself
